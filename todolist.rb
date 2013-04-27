75a8014bfe082b5a93b4f168f9ee1d9b91b80943
Laura Lindeman <lclindeman@gmail.com>
2013-04-27 10:27:21 -0400

    Adding files for to-do list project

---
+++ todolist.rb
@@ -0,0 +1,5 @@
+# add a task
+# cross off a task when completed
+# remove a task b/c you don't want to do it anymore
+# see what uncompleted tasks are on my list
+# see what tasks have been completed
